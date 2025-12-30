cde7e7b2193a29b92bc02ce4db04e5cfaced1a57
Trillion_Impulse <tkdduq2897@naver.com>
2025-12-30 17:15:08 +0900

    feat(app/main.py): 애플리케이션 진입점 main 함수 구현
    - 메인 로거 생성
    - 환경변수 로드
    - 환경변수 검증
    - 크롤러 실행

---
+++ app/main.py
@@ -0,0 +1,43 @@
+from app.config.config import get_crawler_configs, validate_crawler_configs
+from app.utils.logger import get_logger
+from app.crawler.crawler_1 import crawler_1_run
+
+def main() -> None:
+    """
+    애플리케이션의 진입점
+    - 메인 로거
+    - 환경변수 로드
+    - 환경변수 검증
+    - 크롤러 실행
+    """
+
+    logger = get_logger(__name__)
+    logger.info("애플리케이션 실행 시작")
+
+    crawler_name = "CRAWLER_1"
+
+    # 환경변수 로드 및 검증
+    try:
+        logger.info("환경변수 로드 및 검증 시작")
+        config = get_crawler_configs(crawler_name) # 환경변수 키 로드 및 검증
+        validate_crawler_configs(crawler_name) # 환경변수 값 검증
+    except Exception:
+        logger.error("환경변수 로드 및 검증 오류", exc_info=True)
+        return
+    else:
+        logger.info("환경변수 로드 및 검증 성공")
+    
+    # 크롤러 실행
+    try:
+        logger.info(f"{crawler_name} 실행 시작")
+        crawler_1_run(config)
+    except Exception:
+        logger.error(f"{crawler_name} 실행 오류", exc_info=True)
+        return
+    else:
+        logger.info(f"{crawler_name} 실행 성공")
+    
+    logger.info("애플리케이션 실행 종료")
+
+if __name__=="__main__":
+    main()
